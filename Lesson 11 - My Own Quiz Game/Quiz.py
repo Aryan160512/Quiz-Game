77e650d2af71a90cf517eca8719b6e3688bf2363
Aryan160512 <157271425+Aryan160512@users.noreply.github.com>
2025-03-02 09:18:17 +0000

    Add files via upload

---
+++ Lesson 11 - My Own Quiz Game/Quiz.py
@@ -0,0 +1,82 @@
+import pgzrun
+
+WIDTH = 870 
+HEIGHT = 650
+TITLE = 'QUIZ GAME'
+
+marqueeBox = Rect(0, 0, 880, 80)
+questionBox = Rect(0, 0, 650, 150)
+timerBox = Rect(0, 0, 150, 150)
+skipBox = Rect(0, 0, 150, 330)
+
+answerBox1 = Rect(0, 0, 300, 150)
+answerBox2 = Rect(0, 0, 300, 150)
+answerBox3 = Rect(0, 0, 300, 150)
+answerBox4 = Rect(0, 0, 300, 150)
+
+answers = [answerBox1, answerBox2, answerBox3, answerBox4]
+
+marqueeBox.move_ip(0, 0)
+questionBox.move_ip(20, 100)
+timerBox.move_ip(700, 100)
+
+answerBox1.move_ip(20, 270)
+answerBox2.move_ip(370, 270)
+answerBox3.move_ip(20, 450)
+answerBox4.move_ip(370, 450)
+
+skipBox.move_ip(700, 270)
+
+score = 0
+timeLeft = 10
+marqueeMessage = ''
+
+questions = []
+questionIndex = 0
+questionCount = 0
+
+isGameOver = False
+
+def marquee_message():
+    marqueeBox.x -= 2
+    if marqueeBox.right < 0:
+        marqueeBox.left = WIDTH
+
+def readQuestionFile():
+    global questionCount, questions
+
+    #Opening a File in Read Mode
+    questionFile = open('Questions.txt', 'r')
+
+    for line in questionFile:
+        questions.append(line)
+        questionCount += 1
+
+    questionFile.close()
+    print(questions)
+
+def update():
+    marquee_message()
+    
+def draw():
+    global marqueeMessage
+
+    screen.clear()
+    screen.fill('black')
+
+    screen.draw.filled_rect(marqueeBox, 'black')
+    screen.draw.filled_rect(questionBox, 'blue')
+    screen.draw.filled_rect(timerBox, 'green')
+    screen.draw.filled_rect(skipBox, 'yellow')
+
+    for i in answers:
+        screen.draw.filled_rect(i, 'red')
+
+    marqueeMessage = f'Welcome to the Quiz Game. You are at {questionIndex} of {questionCount}.'
+
+    screen.draw.textbox(marqueeMessage, marqueeBox, color = 'white')
+    screen.draw.textbox('SKIP', skipBox, color = 'black', angle = -90)
+    screen.draw.textbox(str(timeLeft), timerBox, color = 'black', shadow = (0.5, 0.5), scolor = 'darkGreen')
+
+readQuestionFile()
+pgzrun.go()
